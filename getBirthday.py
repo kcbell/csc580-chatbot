2173a6e2bceee781610010fc67c0e276f3140eb3
Toshihiro Kuboi <toshikuboi@hotmail.com>
2012-10-31 17:17:21 +0900

    I cleaned up the code..

--- getBirthday.py
+++ getBirthday.py
@@ -1,6 +1,6 @@
 """
 The function to get the birth date..
-Returns a response including birthday when it is fouond.
+Returns a response sentence including birthday when it is found.
 Other wise it the response says 'was born in the past..'
 """
 
@@ -10,17 +10,10 @@ from urllib import urlopen
 
 
 def getBirthday(name): #name = <firstName><space><lastName> or <name>
-    states = {}
-    state = 0
     months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    birthDate = []
     title = name.replace(" ","_") #convert to firstName_lastName
     url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&action=query&titles=" + name
-    #url = "http://en.wikipedia.org/wiki/" + title
     html = urlopen(url).read()
-    #raw = nltk.clean_html(html)
-    #tokens = nltk.word_tokenize(raw)
-    #tokens = html.split("|")
     idx = html.lower().find("birth date")
     if idx > -1:
         text = html[idx:]
@@ -31,7 +24,7 @@ def getBirthday(name): #name = <firstName><space><lastName> or <name>
             print m.group(1), m.group(2), m.group(3)
             year, month, day = m.group(1, 2, 3)
             print month, day, year
-            result = name + " was born on " + months[int(month)] + " " + day + "," + year
+            result = name + " was born on " + months[int(month)] + " " + day + "," + year + "."
         else:
             result = name + " was born in the past.."
     else:
