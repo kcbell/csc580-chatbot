d2cb1c1f36e07fabd19593c4de25d90ec0b4bcc8
kcbell <kcbell@calpoly.edu>
2012-11-04 19:19:29 -0800

    Adding getSubject.py

---
+++ getSubject.py
@@ -0,0 +1,67 @@
+"""
+Function to pull a subject out of a message using some REs and chunking.
+"""
+
+import re, nltk, string
+
+from nltk.corpus import brown
+from nltk.chunk import RegexpParser
+
+punct = '!"#$%&*+,-./:;<=>?@[\]^_`{|}~' + "'"
+
+# over-sensitive
+npGrammar = r"""
+      NP: {(<NP>|<NPS>|<NN>|<NNS>)+} 
+    """
+
+def getPOSTagger():
+    # lazy init
+    if not hasattr(getPOSTagger, 'tagger'):
+        brown_tagged_sents = brown.tagged_sents(categories='news',simplify_tags=True)
+        t0 = nltk.DefaultTagger('NN')
+        t1 = nltk.UnigramTagger(brown_tagged_sents, backoff=t0)
+        getPOSTagger.tagger = nltk.BigramTagger(brown_tagged_sents, backoff=t1)
+    return getPOSTagger.tagger
+
+def getChunker():
+    # lazy init
+    if not hasattr(getChunker, 'chunker'):
+        getChunker.chunker = RegexpParser(npGrammar)
+    return getChunker.chunker
+
+def chunkMsg(msg):
+    tagger = getPOSTagger()
+    sents = nltk.sent_tokenize(msg)
+    words = [sent.split() for sent in sents]
+    tagged = map(tagger.tag, words)
+    chunker = getChunker()
+    chunks = map(chunker.parse, tagged)
+    return chunks
+
+def cleanSubject(subject):
+    s = subject.strip()
+    s = s.replace("'s",'')
+    s = re.sub('\s+', ' ', s)
+    s = string.capwords(s)
+    for c in punct:
+        s = s.replace(c, '')
+    return s
+
+def getSubjects(msg):
+    chunkses = chunkMsg(msg)
+    subjects = []
+    for chunks in chunkses:
+        for chunk in chunks:
+            if hasattr(chunk, 'node') and chunk.node == 'NP':
+                subject = ' '.join([w for (w,t) in chunk])
+                subjects.append(cleanSubject(subject))
+    return subjects
+    
+def main():
+    msg = raw_input()
+    resp = getSubjects(msg)
+    print 'Response:'
+    print resp
+    
+if __name__ == "__main__":
+    main()
