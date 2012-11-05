d06a3666f460717c59adda15320643aab8492743
Toshihiro Kuboi <toshikuboi@hotmail.com>
2012-11-05 17:47:40 +0900

    updated getRelatedTopic to call Jake's function

--- getRelatedTopic.py
+++ getRelatedTopic.py
@@ -4,7 +4,7 @@ Returns a response sentence including birthday when it is found.
 Other wise it the response says 'was born in the past..'
 """
 
-import re, nltk, random
+import re, nltk, random, getSubjectInfo
 
 from urllib import urlopen
 
@@ -35,42 +35,39 @@ def getRelatedTopic(name): #name = <firstName><space><lastName> or <name>
             pageTitle = topic.replace(" ","_") #convert to firstName_lastName
             
             # call Jake's function here
-            result = (pageTitle, name)         #get some fuct from the linked page
-    
+            result = getSubjectInfo.getFacts(name, pageTitle)         #get some fuct from the linked page
+            #result = (name, pageTitle)
+            
     return result
 
 def getLinks(name, maxLink):
-    title = name.replace(" ","_") #convert to firstName_lastName
-    url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&rvsection=0&action=query&redirects=yes&titles=" + title
-    html = urlopen(url).read()
-
     startTag = '[['
     endTag = ']]'
     links = []
-    str = html
-    idx1 = str.find(startTag)
+    
+    title = name.replace(" ","_") #convert to firstName_lastName
+    url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&rvsection=0&action=query&redirects=yes&titles=" + title
+    html = urlopen(url).read()
+    if len(html) > 0: #if the page is found
+        str = html
+    else:   # if the page not found
+        return links  #return empty list
+    
+    idx1 = str.find(startTag)   # find start tag
     count = 0
-	
     while idx1 > -1 and count < maxLink:
-        idx2 = str.find(endTag)
-        link = str[(idx1 + len(startTag)) : idx2]
-        str = str[idx2 + len(endTag):]
-        '''
-        if len(link) > 0:
-            try:
-                links.index(link)
-            except ValueError:
-                links.append(link)
-        '''
-        if len(link) > 0:
-            idx = link.find("|")
+        idx2 = str.find(endTag) # find end tag
+        link = str[(idx1 + len(startTag)) : idx2] #get link
+        str = str[idx2 + len(endTag):] #slide the window
+        if len(link) > 0: #if any some is found
+            idx = link.find("|")  #look for text for display purpose only
             if idx > 0:
-                link = link[:idx]
-            links.append(link.replace("&amp;#039;","'"))
-        idx1 = str.find(startTag)
-        count += 1
+                link = link[:idx] #remove the text
+            links.append(link.replace("&amp;#039;","'")) #append the link to the list
+        idx1 = str.find(startTag) #find next start tag
+        count += 1                #increment the count
         
-    return links
+    return links                  #return the list of links
 
     
 def main():
