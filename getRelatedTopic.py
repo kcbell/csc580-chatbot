17b8e5a491c8e322dea228dc11a4ce9fceabd6bf
Toshihiro Kuboi <toshikuboi@hotmail.com>
2012-11-07 14:42:19 +0900

    Added new stuff

--- getRelatedTopic.py
+++ getRelatedTopic.py
@@ -21,11 +21,15 @@ def getRelatedTopic(name): #name = <firstName><space><lastName> or <name>
         #check to see if the linked page refers back to this page
         while len(links) != 0 and idx == -1:
             topic = random.choice(links)  #randomly picks a topic(link)
+<<<<<<< HEAD
             print "checking related topic " + topic 
             rLinks = getLinks(topic,100) #get links from the linked page
+=======
+            rLinks = getLinks(topic[0],100) #get links from the linked page
+>>>>>>> adding new stuff. Now returns orgSub, newSub, Sentence
             #print rLinks
             for l in rLinks:
-                idx = l.lower().find(name.lower()) #check to see if there is a link to this page
+                idx = l[0].lower().find(name.lower()) #check to see if there is a link to this page
                 if idx > -1:
                     break                    
                 
@@ -33,8 +37,17 @@ def getRelatedTopic(name): #name = <firstName><space><lastName> or <name>
                 links.remove(topic)      #if no link, remove the toic (link)
 
         if idx > -1: #if a topic (link) is found
+<<<<<<< HEAD
             print "found good topic: " + topic
             return topic
+=======
+            relatedSentence = getRelatedSentence(name, topic)
+            pageTitle = topic[0].replace(" ","_") #convert to firstName_lastName
+            
+            # call Jake's function here
+            #result = getSubjectInfo.getFacts(name, pageTitle)         #get some fuct from the linked page
+            result = (name, topic[0], relatedSentence)
+>>>>>>> adding new stuff. Now returns orgSub, newSub, Sentence
             
     return result
 
@@ -44,8 +57,16 @@ def getLinks(name, maxLink):
     links = []
     
     title = name.replace(" ","_") #convert to firstName_lastName
+<<<<<<< HEAD
     url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&rvsection=0&action=query&redirects=yes&titles=" + title
     #print "using url " + url
+=======
+    try:
+        url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&rvsection=0&action=query&redirects=yes&titles=" + title
+    except:
+        return links
+
+>>>>>>> adding new stuff. Now returns orgSub, newSub, Sentence
     html = urlopen(url).read()
     if len(html) > 0: #if the page is found
         str = html
@@ -57,19 +78,58 @@ def getLinks(name, maxLink):
     while idx1 > -1 and count < maxLink:
         idx2 = str.find(endTag) # find end tag
         link = str[(idx1 + len(startTag)) : idx2] #get link
-        str = str[idx2 + len(endTag):] #slide the window
         if len(link) > 0: #if any some is found
             idx = link.find("|")  #look for text for display purpose only
             if idx > 0:
                 link = link[:idx] #remove the text
+<<<<<<< HEAD
             if not link.startswith("File:"):
                 links.append(link.replace("&amp;#039;","'")) #append the link to the list
+=======
+                txt = link[idx+1:]
+            else:
+                txt = ""
+            links.append((link.replace("&amp;#039;","'"),txt)) #append the link to the list
+        str = str[idx2 + len(endTag):] #slide the window
+>>>>>>> adding new stuff. Now returns orgSub, newSub, Sentence
         idx1 = str.find(startTag) #find next start tag
         count += 1                #increment the count
         
     return links                  #return the list of links
 
+def getRelatedSentence(name, topic):
+    title = name.replace(" ","_") #convert to firstName_lastName
+    url = "http://en.wikipedia.org/w/api.php?format=xml&prop=extracts&explaintext=true&action=query&redirects=yes&titles=" + title
+    #url = "http://en.wikipedia.org/w/api.php?format=xml&prop=revisions&rvprop=content&rvsection=0&action=query&redirects=yes&titles=" + title
+
+    target = ""
     
+    try:
+        html = urlopen(url).read()
+    except:
+        return target
+    
+    raw = nltk.clean_html(html)
+    sentences = nltk.sent_tokenize(raw)
+    target = ""
+    for sent in sentences:
+        if len(topic[1]) > 0 and sent.find(topic[1]) > -1:
+            target = sent
+            break
+        elif len(topic[1]) == 0 and sent.find(topic[0]) > -1:
+            target = sent
+            break
+        
+    if len(target) > 0:
+        print target
+    
+    return target
+        
+            
+    
+    
+
+
 def main():
     msg = raw_input()
     resp = getRelatedTopic(msg)
